38faebad5a9ff85cef5db5e52cdb64d9116a416d
logitha <loga848.456@gmail.com>
2016-07-04 14:23:20 +0530

    Create a3.c

---
+++ a3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a[50],i,j,k;
+scanf("%d",&i);
+for(j=0;j<i;j++)
+scanf("%d",&a[i]);
+for(j=0;j<k;j++)
+{
+for(k=1;k<i;k++)
+{
+if((a[j]+a[k]));
+{
+printf("%d%d",a[j],a[k]);
+break;
+}
+}
+}
+return 0;
+}
